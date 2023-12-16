e2e2efb7a84f504c0aff711786699312ff3e633a
Abel Lavieri <abel.lavieri@gmail.com>
2023-12-16 15:46:41 -0400

    Add salutation seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+puts 'Seeding the greetings to the database ...'
+
+['Greetings earthling!', 'Hello gentlefriend!', 'Well hello hello', 'Aloha', 'Salutations!'].each do |greeting|
+  Greeting.find_or_create_by!(display_text: greeting)
+  puts "Created the greeting: #{greeting}."
+end
+
+puts 'Greetings created.'
